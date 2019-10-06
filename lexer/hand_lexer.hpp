0d939c8da0e9a700ecb7d406657bdc123c1321c7
Sersh Dobychin <makasy2@rambler.ru>
2019-10-07 01:14:19 +0300

    Add hand lexer file

---
+++ lexer/hand_lexer.hpp
@@ -0,0 +1,13 @@
+#ifndef _HAND_LEXER_HPP_
+#define _HAND_LEXER_HPP_
+
+namespace Lexer
+{
+    class HandLexer {
+    public:
+	    makeTokens
+		
+    }
+} // namespae Lexer
+
+#endif // _HAND_LEXER_HPP_
